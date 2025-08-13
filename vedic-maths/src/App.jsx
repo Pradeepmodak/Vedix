a571de26c1066e7693914b2b34ac074953b960a5
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2025-08-13 16:59:02 +0530

    Project setup done

--- vedic-maths/src/App.jsx
+++ vedic-maths/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className="text-8xl font-bold underline text-[#4B3621] text-center mt-20">
+      Vedix
+    </h1>
   )
 }
 
